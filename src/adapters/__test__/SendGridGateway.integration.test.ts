7927088fd06c7e2b4f57aedeb5b8f45c986f121d
Cédric Herouard <115704191+Bric66@users.noreply.github.com>
2023-01-13 17:28:32 +0100

    fix sendgrid template id

--- src/adapters/__test__/SendGridGateway.integration.test.ts
+++ src/adapters/__test__/SendGridGateway.integration.test.ts
@@ -30,7 +30,7 @@ describe('Integration - SendGridGateway', () => {
             email: 'benjamin@hello.com',
             message: 'Hello, j\'adore l\'app'
         });
-        expect(spyOnSendMethod).toHaveBeenCalledWith({"dynamicTemplateData": {"message": "Hello, j'adore l'app"}, "from": {"email": "cedric@hello.com", "name": "Sweet Dev"}, "templateId": "d-f6e42c62ae494789965870a9af270a2e", "to": "cedric@hello.com"});
+        expect(spyOnSendMethod).toHaveBeenCalledWith({"dynamicTemplateData": {"message": "Hello, j'adore l'app"}, "from": {"email": "cedric@hello.com", "name": "Sweet Dev"}, "templateId": "d-3446010a8e8c4e18859dac0eae658c0a", "to": "cedric@hello.com"});
     })
 
     it('Should send a recovery code', async () => {
@@ -40,6 +40,6 @@ describe('Integration - SendGridGateway', () => {
             userName: 'user',
             resetLink: 'http://localhost:3000'
         })
-        expect(spyOnSendMethod).toHaveBeenCalledWith({"dynamicTemplateData": {"name": "user", "reset_password_link": "http://localhost:3000"}, "from": {"email": "cedric@hello.com", "name": "Sweet Dev"}, "templateId": "d-84cfb4f9b5c24788b11df0f56a9acdba", "to": "hello@hello.com"})
+        expect(spyOnSendMethod).toHaveBeenCalledWith({"dynamicTemplateData": {"name": "user", "reset_password_link": "http://localhost:3000"}, "from": {"email": "cedric@hello.com", "name": "Sweet Dev"}, "templateId": "d-19cad47b2933445d9b9a2db0eba28085", "to": "hello@hello.com"})
     })
 })
